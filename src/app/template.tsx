967ca25a8ad308adc3173ba31775cb2d717576a9
Acuarius-prog <andrescastilho@hotmail.com>
2026-08-19 17:11:59 -0500

    Add files via upload

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,11 +1,11 @@
 "use client";
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
-    <div className="page-anim">
+    <>
       <div className="page-veil" aria-hidden="true">
         <img src="/logo.jpg" alt="" className="page-veil-logo" />
       </div>
-      {children}
-    </div>
+      <div className="page-anim">{children}</div>
+    </>
   );
 }
